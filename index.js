0310f67711e4e18084573c7cf3b4763a66d19e74
C Lewis <casserole.lewis@gmail.com>
2023-01-28 15:01:33 -0500

    Updated styling and began functionaliry
    Changed position of ul in HTML
    updated CSS styling: color palette and widths
    JS: began functionality

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//TODO
+//TODO W3 Schools accordion menu tutorial: https://www.w3schools.com/howto/howto_js_accordion.asp
+
+
+//TODO define accordian variable 
+
+let acc = document.getElementsByClassName('accordion');
+
+//TODO loop through accordion items
+
+for (let i = 0; i < acc.length; i++) {
+    console.log(acc[i]);
+    acc[i].addEventListener("click", function() {
+        //TODO Toggle between adding and removing the "active" class
+       
+    this.classList.toggle("active");
+    
+        //TODO Toggle between hiding and showing the active panel 
+    let panel = this.nextElementSibling;
+    
+        console.log(panel);
+
+    if (panel.style.display === "block") {
+          panel.style.display = "none";
+    } else {
+          panel.style.display = "block";
+    }
+      });
+    } 
+
+  */  
