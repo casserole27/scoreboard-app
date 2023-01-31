80acc000dfcf110799f2efe88e5cf78c03cd7d5d
C Lewis <casserole.lewis@gmail.com>
2023-01-31 11:48:11 -0500

    Initial JS accordion functionality

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 
 
+function showRules(){
+
+    let panel = document.getElementById('panel');
+    // console.log(panel);
+
+    if (panel.style.display === "none") {
+        panel.style.display = "block";
+    } else {
+        panel.style.display = "none";
+    }
+    
+}
 
 
 
+/*
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0;
+    countEl.textContent = 0;
+    
+}
+*/
 
 
 
